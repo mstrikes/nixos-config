ff5a478f7db761dd89f887ed0fa3a1526b147d24
Matan Dery <matand103@gmail.com>
2022-10-15 18:53:39 +0300

    make discord and firefox use same nss version

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,8 @@
 self: super: {    
-    discord = super.discord.overrideAttrs (oldAttrs: {
+    discord = (super.discord.overrideAttrs (oldAttrs: {
         src = builtins.fetchTarball {
             url = "https://discord.com/api/download?platform=linux&format=tar.gz";
             sha256 = "0qaczvp79b4gzzafgc5ynp6h4nd2ppvndmj6pcs1zys3c0hrabpv";
         };
-        nss = self.nss;
-    });
+    })).override { nss = self.nss_latest; };
 }
--- users/matan/home.nix
+++ users/matan/home.nix
@@ -29,7 +29,7 @@
             main = {
                 term = "xterm-256color";
 
-                font = "JetBrains Mono:size=11";
+                font = "JetBrains Mono:size=12";
                 dpi-aware = "yes";
             };
 
@@ -39,7 +39,58 @@
             };
             
             cursor = {
-                style = "beam";
+                style = "block";
+                color = "2e3440 d8dee9";
+                blink = false;
+                beam-thickness = 1.5;
+            };
+
+            colors = {
+                alpha = 1.0;
+                foreground = "d8dee9";
+                background = "2e3440";
+                
+                # Normal/regular colors (color palette 0-7)
+                regular0 = "3b4252";
+                regular1 = "bf616a";
+                regular2 = "a3be8c";
+                regular3 = "ebcb8b";
+                regular4 = "81a1c1";
+                regular5 = "b48ead";
+                regular6 = "88c0d0";
+                regular7 = "e5e9f0";
+                
+                # Bright colors (color palette 8-15)
+                bright0 = "4c566a";
+                bright1 = "bf616a";
+                bright2 = "a3be8c";
+                bright3 = "ebcb8b";
+                bright4 = "81a1c1";
+                bright5 = "b48ead";
+                bright6 = "8fbcbb";
+                bright7 = "eceff4";
+                
+                # dimmed colors (see foot.ini(5) man page)
+                dim0 = "373e4d";
+                dim1 = "94545d";
+                dim2 = "809575";
+                dim3 = "b29e75";
+                dim4 = "68809a";
+                dim5 = "8c738c";
+                dim6 = "6d96a5";
+                dim7 = "aeb3bb";
+                
+                # The remaining 256-color palette
+                # 16 = <256-color palette #16>
+                # ...
+                # 255 = <256-color palette #255>
+                
+                # Misc colors
+                # selection-foreground=<inverse foreground/background>
+                # selection-background=<inverse foreground/background>
+                # jump-labels=<regular0> <regular3>
+                # urls=<regular3>
+                # scrollback-indicator=<regular0> <bright4>
             };
         };
     };
@@ -60,5 +111,7 @@
         };
     };
 
-    programs.firefox.enable = true;
+    programs.firefox = {
+        enable = true;
+    };
 }

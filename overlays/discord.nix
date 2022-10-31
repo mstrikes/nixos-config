8c8206c937528776fb9869765016e4bec155007d
Matan Dery <matand103@gmail.com>
2022-10-31 09:30:18 +0200

    update discord client version

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,8 +1,8 @@
-self: super: {    
+let version = "0.0.21"; in self: super: {    
     discord = (super.discord.overrideAttrs (oldAttrs: {
         src = builtins.fetchTarball {
-            url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-            sha256 = "0qaczvp79b4gzzafgc5ynp6h4nd2ppvndmj6pcs1zys3c0hrabpv";
+            url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+            sha256 = "1pw9q4290yn62xisbkc7a7ckb1sa5acp91plp2mfpg7gp7v60zvz";
         };
     })).override { nss = self.nss_latest; };
 }
